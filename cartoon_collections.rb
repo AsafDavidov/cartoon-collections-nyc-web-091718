6712f722e13553fdfa6c14a11c50b298cd53032e
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 20:57:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index do |dwarf|
-    puts "#{dwarf.to_s}"
+  dwarves.each_with_index do |dwarf,index|
+    puts "#{index+1}. #{dwarf.to_s}"
   end
 end
 
